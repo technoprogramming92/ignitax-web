55b0f6fe11edc7a5d38e40d28a1212abcb085e62
Rachit Trivedi <114496132+technoprogramming92@users.noreply.github.com>
2025-02-27 01:55:24 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  output: "static", // Change to 'server' if using SSR
+  site: "https://your-site.com", // Change to your actual domain
+});
